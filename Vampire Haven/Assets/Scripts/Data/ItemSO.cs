8271d18b5c8496f0022f2ff19fb2f11b55fe849b
hkspence <hkspence@ncsu.edu>
2026-04-15 15:47:22 -0400

    [Implementation] Setting up item/equipment management

---
+++ Vampire Haven/Assets/Scripts/Data/ItemSO.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Item scriptable object that holds information that we can load
+/// into various items
+/// </summary>
+[CreateAssetMenu(fileName = "New Item")]
+public class ItemSO : MonoBehaviour
+{
+    public string itemName;
+    [TextArea] public string itemDescription;
+    public Sprite icon;
+    public bool isGold;
+
+    // Stats that can be buffed/changed with items including Weapons, Potions, Armor, and Abilities
+    [Header("Stats")]
+    public int maxHealth;
+    public int currentHealth;
+}
