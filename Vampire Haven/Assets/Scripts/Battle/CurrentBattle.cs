9872f9b0fdc356b320eb6f4467d637135366f971
hkspence <hkspence@ncsu.edu>
2026-04-15 12:55:11 -0400

    [Implementation] Fixed enemy not disappearing when defeated in battle based on playtesting feedback

---
+++ Vampire Haven/Assets/Scripts/Battle/CurrentBattle.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+/// <summary>
+/// Holds information about the current battle
+/// </summary>
+public class CurrentBattle : MonoBehaviour
+{
+    public static string enemyID;
+}
--- Vampire Haven/Assets/Scripts/Battle/Enemy.cs
+++ Vampire Haven/Assets/Scripts/Battle/Enemy.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
+    public string enemyID;
     public int maxHP = 15;
     public int currentHP;
     public int attackPower = 3;
+
+    private void Start()
+    {
+        // Get rid of the enemy in battle
+        if (EnemyTracker.defeatedEnemies.Contains(enemyID))
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void Awake()
     {
