3d2acc5d97c3ed1e06cf384bd4365fd26c8a54d5
hkspence <hkspence@ncsu.edu>
2026-04-29 13:24:30 -0400

    [Debug] Fixing GameConstants.cs issue

--- Vampire Haven/Assets/Scripts/Core/GameConstants.cs
+++ Vampire Haven/Assets/Scripts/Core/GameConstants.cs
@@ -1,17 +1,17 @@
 /// <summary>
 /// Constants that will be used by the project
 /// </summary>
-//###################################################################################//
+public static class GameConstants {
+    // Scene names
+    public const string TITLE_SCENE = "TitleScreenScene";
+    public const string CLASS_SELECT_SCENE = "ClassSelectScene";
+    public const string INTRO_SCENE = "IntroScene";
+    public const string BATTLE_SCENE = "BattleEncounterScene";
+    public const string MAIN_SCENE = "AlphaScene";
 
-// Scene names
-public const string TITLE_SCENE = "TitleScreenScene";
-public const string CLASS_SELECT_SCENE = "ClassSelectScene";
-public const string INTRO_SCENE = "IntroScene";
-public const string BATTLE_SCENE = "BattleEncounterScene";
-public const string MAIN_SCENE = "AlphaScene";
+    // Saving 
+    public const int MAX_SAVE_SLOTS = 3;
 
-// Saving 
-public const int MAX_SAVE_SLOTS = 3;
-
-// Inventory
-public const int MAX_INVENTORY_SLOTS = 8;
+    // Inventory
+    public const int MAX_INVENTORY_SLOTS = 8;
+}
